4223d90c216fe0b0a391a2f241236d534c7408c6
EC2 Default User <ec2-user@ip-172-31-55-216.ec2.internal>
2023-03-28 10:24:44 +0000

    Changes

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+# Reference DOC- https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_stepfunctions-readme.html
--- lib/aws-steps-stack.ts
+++ lib/aws-steps-stack.ts
@@ -1,16 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
+//Reference DOC: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_stepfunctions-readme.html
+
+import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {join} from 'path';
+import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as sfn from "aws-cdk-lib/aws-stepfunctions";
+import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
 
-export class AwsStepsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AwsStepsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsStepsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_16_X,
+            code: Code.fromAsset(join(__dirname, '..','services','hello')),
+            handler: 'hello.main'
+        })
+        
+    const taskFn1 = new tasks.LambdaInvoke(this, 'Function 1',{
+      lambdaFunction: helloLambda,
+      // Lambda's result is in the attribute `guid`
+      outputPath: '$.guid',
+    })
+  
+  const taskFn2 = new tasks.LambdaInvoke(this, 'Function 2',{
+      lambdaFunction: helloLambda,
+      // Lambda's result is in the attribute `guid`
+      inputPath: '$.guid',
+      outputPath: '$.status'
+    })
+  
+  const taskFn3 = new tasks.LambdaInvoke(this, 'Function 3',{
+      lambdaFunction: helloLambda,
+      // Lambda's result is in the attribute `guid`
+      inputPath: '$.guid',
+      outputPath: '$.status'
+    })
+  
+  const taskFn4 = new tasks.LambdaInvoke(this, 'Function 4',{
+      lambdaFunction: helloLambda,
+      // Lambda's result is in the attribute `guid`
+      inputPath: '$.guid',
+      outputPath: '$.status'
+    })
+  
+  const definition = taskFn1
+    .next(taskFn2)
+    .next(taskFn3)
+    .next(taskFn4);
+  
+  
+  new sfn.StateMachine(this, 'StateMachine',{
+    definition,
+    
+  })
+   
   }
 }
